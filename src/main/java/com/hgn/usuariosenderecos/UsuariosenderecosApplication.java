caa143eba5639c19cd65a31f6469060d9a51e683
humbertoguerrer <humberto.guerrer@gmail.com>
2021-05-05 21:04:23 -0300

    Projeto criado

---
+++ src/main/java/com/hgn/usuariosenderecos/UsuariosenderecosApplication.java
@@ -0,0 +1,13 @@
+package com.hgn.usuariosenderecos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosenderecosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosenderecosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hgn/usuariosenderecos/UsuariosenderecosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgn.usuariosenderecos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosenderecosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
